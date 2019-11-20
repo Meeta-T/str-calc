ff80bfe641ae03af3011acde2c15abf6e8f80340
Meeta-T <thacker.meeta@gmail.com>
2019-11-20 23:12:33 +0530

    Step 6: Test failed - ignore numbers bigger than 1000

--- src/test/java/com/kata/StringCalculatorTest.java
+++ src/test/java/com/kata/StringCalculatorTest.java
@@ -31,7 +31,8 @@ public class StringCalculatorTest {
 	        new Object[] { "3,4,6",13 },
 	        new Object[] { "1,2,3,4,5,6,7,8,9", 45 },
 	        new Object[] { "1\n2,3", 6 },
-	        new Object[] { "//;\n1;2", 3 }
+	        new Object[] { "//;\n1;2", 3 },
+	        new Object[] { "1001, 2", 2 }
 	    };
 	}
 	@Test
