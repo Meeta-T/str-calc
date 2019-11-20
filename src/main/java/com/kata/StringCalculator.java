498a50af311021643038de892a1646c996baff6b
Meeta-T <thacker.meeta@gmail.com>
2019-11-20 12:11:09 +0530

    Step 1 : Refactoring

--- src/main/java/com/kata/StringCalculator.java
+++ src/main/java/com/kata/StringCalculator.java
@@ -3,11 +3,12 @@ package com.kata;
 public class StringCalculator {
 	Integer add(String numbers){
 		Integer result = new Integer(0);
-		if(numbers.equals(""))
-			return 0;
-		String[] arrNumbers = numbers.split(",");
-		for (String string : arrNumbers) {
-			result = result + Integer.parseInt(string);
+		String seperator = ",";
+		if(!numbers.isEmpty()){
+			String[] arrNumbers = numbers.split(seperator);
+			for (String string : arrNumbers) {
+				result = result + Integer.parseInt(string);
+			}
 		}
 		return result;
 	}
